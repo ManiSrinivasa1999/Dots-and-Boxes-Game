d865b61c29003855e969211d059e05f600a9002d
manisrinivasa <manisrinivasa1999@gmail.com>
2020-01-15 21:09:58 +0530

    initialised game view  structure

--- assets/js/game.ts
+++ assets/js/game.ts
@@ -1,32 +1,68 @@
 (
   document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
+      // config constants
+      const COMPUTER = 0;
+      const TWOPLAYER = 1;
+      const FIVEBOX = [5, 5];
+      const EIGHTBOX = [8, 8];
+      const ELEVENBOX = [11, 11];
       const PlAYNOW = document.getElementById(
           "playnow") as HTMLInputElement;
-      const selectPlayerControls = document.getElementsByClassName(
-        "select-player") as HTMLCollectionOf<HTMLLIElement>;
+      const SELECTPLAYERCONTROLS = document.getElementsByClassName(
+          "select-player") as HTMLCollectionOf<HTMLLIElement>;
+      const SELECTBOXESCONTROLS = document.getElementsByClassName(
+          "select-boxes") as HTMLCollectionOf<HTMLLIElement>;  
+      const GAMEVIEWPAGE = document.getElementById(
+          "game-view") as HTMLDivElement; 
+      const GAMEVIEWPAGEBOX = document.getElementById(
+          "game-view-box") as HTMLDivElement;
+      const SELECTIONMAP = {
+        0:COMPUTER,
+        1:TWOPLAYER,
+      };
+      const BOXESMAP = {
+        0:FIVEBOX,
+        1:EIGHTBOX,
+        2:ELEVENBOX,
+      };      
       const MODEL = {
         gameconfig: {
-          noOfPlayers: 1,
-          sizeOfBoxes: [5, 4],
+          noOfPlayers: 0, // 0-computer 1-2player
+          sizeOfBoxes: FIVEBOX, // 0-5X5 1-8x8 2-11x11
+          // 6 dots in a row and 6 dots in a column - 0 is index value
         },
       };
       const STARTVIEW = {
         render: () => {
-          for (let i = 0; i < selectPlayerControls.length; i += 1) {
+          for (let i = 0; i < SELECTPLAYERCONTROLS.length; i += 1) {
             if (MODEL.gameconfig.noOfPlayers === i) {
-              selectPlayerControls[i].classList.add("is-active");
+              SELECTPLAYERCONTROLS[i].classList.add("is-active");
             } else {
-              selectPlayerControls[i].classList.remove("is-active");
+              SELECTPLAYERCONTROLS[i].classList.remove("is-active");
             }
-          }                   
+          }  
+          for (let j = 0; j < SELECTBOXESCONTROLS.length; j += 1) {
+            // FIX Array Comparison
+            if (MODEL.gameconfig.sizeOfBoxes.every( e => BOXESMAP[j].includes(e))) { 
+              SELECTBOXESCONTROLS[j].classList.add("is-active");
+            } else {
+              SELECTBOXESCONTROLS[j].classList.remove("is-active");
+            }
+          }
         },
         init: () => {          
-          for (let i = 0; i < selectPlayerControls.length; i += 1) {
-            selectPlayerControls[i].onclick = () => {
+          for (let i = 0; i < SELECTPLAYERCONTROLS.length; i += 1) {
+            SELECTPLAYERCONTROLS[i].onclick = () => {
               CONTROLLER.handlePlayerSelection(i);
             };
           }
+          for (let j = 0; j < SELECTBOXESCONTROLS.length; j += 1) {
+            SELECTBOXESCONTROLS[j].onclick = () => {
+              CONTROLLER.handleBoxesSelection(j);
+            };
+          }
+          PlAYNOW.onclick = CONTROLLER.handlePlayButton;
         },
       };
       const GAMEVIEW = {
@@ -34,13 +70,55 @@
 
         },
         init: () => {
-
+          const HEIGHT = MODEL.gameconfig.sizeOfBoxes[0];
+          const WIDTH = MODEL.gameconfig.sizeOfBoxes[1];
+          GAMEVIEWPAGEBOX.innerHTML = ``;
+          for (let h = 0; h < WIDTH-1; h += 1) {
+            GAMEVIEWPAGEBOX.innerHTML += 
+            `
+            <div class="dots"></div>
+            <canvas id="myCanvas" width="20vmin" height="10vmin"></canvas>
+            `;
+          }
+          GAMEVIEWPAGEBOX.innerHTML +=
+          `<div class="dots"></div><br>`; 
+          for (let v = 0; v < HEIGHT-1; v += 1) {
+            for (let w = 0; w < WIDTH-1; w += 1) {
+              GAMEVIEWPAGEBOX.innerHTML += 
+              `
+              <canvas id="myCanvas" width="10vmin" height="20vmin"></canvas>
+              <canvas width="20vmin" height="20vmin"></canvas>    
+              `;
+            }
+            GAMEVIEWPAGEBOX.innerHTML += 
+            `<canvas id="myCanvas" width="10vmin" height="20vmin"></canvas><br>`;
+            for (let k = 0; k < WIDTH-1; k += 1) {
+              GAMEVIEWPAGEBOX.innerHTML += 
+              `
+              <div class="dots"></div>
+              <canvas id="myCanvas" width="20vmin" height="10vmin"></canvas>
+              `;
+            }
+            GAMEVIEWPAGEBOX.innerHTML +=
+            `<div class="dots"></div><br>`;                       
+          }                  
         },
       };
       const CONTROLLER = {
-        handlePlayerSelection: (index: number) => {
+        handlePlayButton: () => {
+          const STARTVIEWPAGE = document.getElementById(
+            "start-view") as HTMLDivElement;
+          STARTVIEWPAGE.classList.add("hide");
+          GAMEVIEWPAGE.classList.remove("hide");
+          GAMEVIEW.init();      
+        },
+        handlePlayerSelection: (indexOfPlayers: number) => {
           // updates noofplayers
-          MODEL.gameconfig.noOfPlayers = index;
+          MODEL.gameconfig.noOfPlayers = SELECTIONMAP[indexOfPlayers];
+          STARTVIEW.render();
+        },
+        handleBoxesSelection: (indexOfBoxes: number) => {
+          MODEL.gameconfig.sizeOfBoxes = BOXESMAP[indexOfBoxes];
           STARTVIEW.render();
         },
         init: () => {
